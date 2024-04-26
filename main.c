36e8fe941a11be7c982173fce9f85a5348a50088
cybersecurity <102185842+malikehack@users.noreply.github.com>
2024-04-26 22:21:22 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() /**Ecrire un programme qui affiche un triangle rectangle isocèle formé d’étoiles de N lignes.**/
+{
+   unsigned int N, i, j ;
+    printf("Tracage d'un triangle isocele\n");
+    printf("Donnez le nombre de ligne\n");
+    scanf("%u",&N);
+
+    for(i=1;i<=N;i++){
+            for(j=1;j<=i+1-1;j++)
+                printf("* ");
+                printf("\n");
+
+    }
+          return 0;
+}
